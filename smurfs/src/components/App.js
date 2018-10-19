07ab1273f17090679af32ce375129d4e15bce5a1
Timothy Hoang <timh1203@gmail.com>
2018-10-19 19:45:39 -0400

    Add webAuth and return results

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { getSmurfs } from "../actions";
 import { Container } from "reactstrap";
 import { Auth0Lock } from 'auth0-lock';
+import auth0 from 'auth0-js';
 
 /*
  to wire this component up you're going to need a few things.
@@ -16,8 +17,28 @@ import { Auth0Lock } from 'auth0-lock';
 
 const lock = new Auth0Lock(
   process.env.REACT_APP_CLIENT_ID,
-  process.env.REACT_APP_DOMAIN_URL
+  process.env.REACT_APP_DOMAIN_URL,
 );
+
+var webAuth = new auth0.WebAuth({
+  domain: process.env.REACT_APP_DOMAIN_URL,
+  clientID: process.env.REACT_APP_CLIENT_ID,
+  redirectUri: 'http://localhost:9000'
+});
+
+webAuth.parseHash((err, authResult) => {
+  if (authResult) {
+    console.log("authResult", authResult);
+    // Save the tokens from the authResult in local storage or a cookie
+    let expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime())
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("expires_at", expiresAt);
+  }
+  else if (err) {
+    // Handle errors
+    console.log(err);
+  }
+});
 
 class App extends Component {
   render() {
